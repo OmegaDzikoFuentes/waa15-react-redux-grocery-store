fc126099b6551d2da2562d8cb1f68f3d1f869469
Omega Dziko F. <megaoy@yahoo.com>
2024-11-20 13:18:44 -0500

    store functioning produce added

---
+++ src/store/store.js
@@ -0,0 +1,30 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import produceReducer from './produce.js';
+
+
+const rootReducer = combineReducers({
+     produce: produceReducer
+})
+
+
+let enhancer;
+
+if (import.meta.env.MODE !== "production") {
+    const logger = (await import("redux-logger")).default;
+    const composeEnhancers =
+        typeof window === "object" &&
+   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+         ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    trace: true })
+          : compose;
+      enhancer =
+     composeEnhancers(applyMiddleware(logger))
+}
+
+const configureStore = (preloadedstate) => {
+    return createStore(rootReducer,
+        preloadedstate, enhancer);
+};
+
+export default configureStore;
